ec5badb540b505256eb5d665b91451d4a0bcf8e0
Oh-Hoa-Yang <ohhaoyang1@gmail.com>
2023-05-20 01:30:39 +0800

    Addon SearchEngine & briefly frame module2

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,9 +1,17 @@
 <nav class="navbar shadow-sm fixed-top bg-white">
 	<div class="container-fluid">
 		<button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive"><i class="fa-solid fa-bars fa-xl"></i></button>
-		<a class="navbar-brand" href="./">
+		<a class="navbar-brand d-none d-lg-flex" href="./">
 			FK-EduSearch
 		</a>
+
+		<form action="" class="d-none d-lg-flex ms-5" method="get">
+			<div class="input-group">
+				<input class="form-control" type="text" placeholder="Search">
+				<button class="btn btn-outline-info" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+			</div>
+		</form>
+
 		<div class="nav-item dropdown">
 			<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 				<i class="fa-solid fa-user fa-xl"></i>
@@ -19,11 +27,20 @@
 
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
 	<div class="offcanvas-header">
-		<h5 class="offcanvas-title" id="offcanvasResponsiveLabel">Menu</h5>
+		<h5 class="offcanvas-title" id="offcanvasResponsiveLabel">
+			<span class="d-none d-lg-flex">Menu</span>
+			<span class="d-lg-none">FK-EduSearch</span>
+		</h5>
 		<button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
 	</div>
 	<div class="offcanvas-body">
 		<nav class="nav nav-pills flex-column">
+			<form action="" class="d-" method="get">
+				<div class="input-group">
+					<input class="form-control" type="text" placeholder="Search">
+					<button class="btn btn-outline-info" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+				</div>
+			</form>
 			<a class="nav-link" id="discussion" href="./">Discussion</a>
 			<?php
 			if (isset($_SESSION["admin"])) :
--- module2.php
+++ module2.php
@@ -11,16 +11,27 @@ require "./Middleware/Authenticate.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>AnalyticsManagement/Admin-UI</title>
 	<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
-  <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
 	<link rel="stylesheet" href="./node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css">
 	<link rel="stylesheet" href="./node_modules/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css">
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
 	<link href="https://cdn.datatables.net/v/bs5/jq-3.6.0/jszip-2.5.0/dt-1.13.4/b-2.3.6/b-colvis-2.3.6/b-html5-2.3.6/b-print-2.3.6/datatables.min.css" rel="stylesheet" />
+    <!-- Bootstrap 5 CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+    <style>
+    .post {
+      margin-bottom: 20px;
+    }
+
+    .comment {
+      margin-left: 30px;
+    }
+  </style>
 </head>
 
 <body>
 <?php require "layouts/navbar.php" ?>
-	<div class="row mt-5">
+	<div class="row" style="margin-top: 94px;">
 		<div class="col">
 	
 		</div>
@@ -36,17 +47,35 @@ require "./Middleware/Authenticate.php";
 	</div>
     -->
 	<!-- No Changes Above -->
-    <div class="input-group">
-    <div class="form-outline">
-        <input id="search-input" type="search" id="form1" class="form-control" />
-        <label class="form-label" for="form1">Search</label>
-    </div>
-    <button id="search-button" type="button" class="btn btn-primary">
-        <i class="fas fa-search"></i>
-    </button>
-    </div>
-	
+    <!--Search Engine-->
+<!--
+    <input type="text" class="form-control mt-4 ">
+-->
     
+<!--Start -->
+    <div class="container">
+    <div class="post">
+      <h2>ASP.NET Core updates in .NET 8 Preview 4 - .NET Blog</h2>
+      <p class="container"><a href="#">https://devblogs.microsoft.com/dotnet/asp-net-core-updates-in-dotnet-8-preview-4/</a></p>
+      <button type="button" class="btn btn-light like-btn" ><i class="fa-light fa-thumbs-up"></i></button>
+      <span class="likes-count">0</span> Likes
+      <button type="button" class="btn btn-light like-btn" ><i class="fa-regular fa-comment"></i></button>
+      <span class="likes-count">0</span> Comments
+      <hr>
+      <h3>Comments</h3>
+      <div class="comments-container">
+        <!-- Comments will be added dynamically here -->
+      </div>
+      <form id="comment-form">
+        <div class="mb-3">
+          <label for="comment-input" class="form-label">Leave a comment:</label>
+          <input type="text" class="form-control" id="comment-input" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+<!--End-->
 
 
 
@@ -62,15 +91,37 @@ require "./Middleware/Authenticate.php";
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/v/bs5/jq-3.6.0/jszip-2.5.0/dt-1.13.4/b-2.3.6/b-colvis-2.3.6/b-html5-2.3.6/b-print-2.3.6/datatables.min.js"></script>
 	<script src="./resources/js/datatables.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 	<script>
+        // Like button functionality
+        const likeButton = document.querySelector('.like-btn');
+        const likesCount = document.querySelector('.likes-count');
+        let likeCount = 0;
 
-    const searchButton = document.getElementById('search-button');
-    const searchInput = document.getElementById('search-input');
-    searchButton.addEventListener('click', () => {
-    const inputValue = searchInput.value;
-    alert(inputValue);
-    });
+        likeButton.addEventListener('click', () => {
+        likeCount++;
+        likesCount.textContent = likeCount;
+        });
 
+        // Comment functionality
+        const commentForm = document.getElementById('comment-form');
+        const commentsContainer = document.querySelector('.comments-container');
+
+        commentForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const commentInput = document.getElementById('comment-input');
+        const commentText = commentInput.value.trim();
+
+        if (commentText !== '') {
+            const commentElement = document.createElement('div');
+            commentElement.classList.add('comment');
+            commentElement.textContent = commentText;
+            commentsContainer.appendChild(commentElement);
+
+            commentInput.value = '';
+        }
+        });
 	</script>
 </body>
 
