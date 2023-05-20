40d5595486c5c1a737338c6050d293b04403f8ac
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-20 21:11:30 +0800

    update expert id

--- Middleware/UserRole.php
+++ Middleware/UserRole.php
@@ -4,7 +4,7 @@ if ($result["UserRole"] == 0) {
 	$_SESSION["id"] = $result["StaffID"];
 } elseif ($result["UserRole"] == 1) {
 	$_SESSION["expert"] = $result["UserRole"];
-	$_SESSION["id"] = $result["StaffID"];
+	$_SESSION["id"] = $result["ExpertID"];
 } elseif ($result['UserRole'] == 2) {
 	$_SESSION["staff"] = $result["UserRole"];
 	$_SESSION["id"] = $result["StaffID"];
