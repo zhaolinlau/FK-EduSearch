feeb09001f6865ff0582b26c846f2cf1aba6651b
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-21 05:06:29 +0800

    remove datatable.js

--- user_list.php
+++ user_list.php
@@ -169,7 +169,6 @@ require "./Middleware/AdminAuth.php";
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/v/bs5/jq-3.6.0/jszip-2.5.0/dt-1.13.4/b-2.3.6/b-colvis-2.3.6/b-html5-2.3.6/b-print-2.3.6/datatables.min.js"></script>
-	<script src="./resources/js/datatables.js"></script>
 	<script>
 		document.getElementById("user_list").classList.add("active");
 		$.fn.dataTable.Buttons.defaults.dom.button.className =
