18b9e4bb5a24eb82b7bc58322dc813f90f999580
Zhaolin Lau <zhaolinlau@gmail.com>
2023-06-02 18:12:48 +0800

    add create post function

---
+++ controllers/CreatePostController.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../config/db.php';
+
+try {
+  session_start();
+
+  $stmt = $conn->prepare('INSERT INTO post (UserID, PostTitle, PostContent, PostCategory) VALUES (:user_id, :post_title, :post_content, :post_category)');
+
+  $stmt = bindParam(':user_id', $_POST['user_id']);
+  $stmt = bindParam(':post_title', $_POST['post_title']);
+  $stmt = bindParam(':post_content', $_POST['post_content']);
+  $stmt = bindParam(':post_category', $_POST['post_category']);
+
+  $stmt->execute();
+
+  $_SESSION['posted'] = 'You have created a post successfully!';
+
+  header('location: ../');
+
+} catch (PDOException $e) {
+
+  echo $e->getMessage();
+
+}
+
+$conn = null;
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require "./Middleware/Authenticate.php";
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>FK-EduSearch | Discussion</title>
+	<title>FK-EduSearch | Discussion Board</title>
 	<link rel="shortcut icon" href="./resources/img/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
@@ -17,63 +17,88 @@ require "./Middleware/Authenticate.php";
 
 <body>
 	<?php require "layouts/navbar.php" ?>
-	<div class="row mt-5">
-		<div class="col">
 
-		</div>
-	</div>
-	<div class="row" style="margin-top: 94px;">
-		<div class="col">
-		</div>
-	</div>
-
-	<div class="">
-		<div class="col-9 mx-auto" style="margin-right: 0;">
-			<div> <!--Discussion Board and search bar-->
+	<div class="container mt-5">
+		<div class="row g-5">
+			<div class="col-12">
 				<h3>Discussion Board</h3>
-				<br><br>
-				<div class="row g-3">
-					<div class="col justify-content-end d-flex">
-						<button class="btn btn-primary rounded-5" data-bs-toggle="modal" data-bs-target="#post_form">
-							<i class="fa-regular fa-plus"></i>
-							Create Post
-						</button>
+			</div>
+
+			<?php if(isset($_SESSION['posted'])) : ?>
+			<div class="col-12">
+				<div class="alert alert-success alert-dismissible fade show" role="alert">
+					<?php echo $_SESSION['posted'] ?>
+					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+				</div>
+			</div>
+		<?php endif; ?>
+
+			<div class="col-12">
+				<button class="float-end btn btn-primary rounded-5" data-bs-toggle="modal" data-bs-target="#post_form">
+					<i class="fa-regular fa-plus"></i>
+					Create Post
+				</button>
+
+				<div class="dropdown d-flex align-items-center justify-content-center text-center">
+					<a class="dropdown-toggle d-flex align-items-center text-decoration-none text-black" href="#" role="button" id="filterDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="gg-format-center"></i> &nbsp; Filter by Post Status
+					</a>
+					<ul class="dropdown-menu" aria-labelledby="filterDropdown">
+						<li><a class="dropdown-item" href="#" onclick="">Accepted</a></li>
+						<li><a class="dropdown-item" href="#" onclick="">Revised</a></li>
+						<li><a class="dropdown-item" href="#" onclick="">Completed</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-12">
+				<div class="card border-0 shadow">
+					<div class="card-header bg-white">
+						<h5 class="card-title fw-semibold">Post Title</h5>
+					</div>
+					<div class="card-body">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eos non commodi obcaecati molestias qui ab? Sed inventore, nam consequuntur dicta dignissimos eaque doloremque, tempore qui necessitatibus, sequi assumenda ducimus?</p>
+					</div>
+					<div class="card-footer bg-white">
+						<button class="btn btn-outline-primary"><i class="fa-solid fa-thumbs-up"></i> Like</button>
+						<button class="btn btn-light"><i class="fa-solid fa-comment"></i> Comment</button>
+						<span class="badge bg-info fs-6">Post Status: Placeholder</span>
 					</div>
 				</div>
 			</div>
 
-			<div class="dropdown d-flex align-items-center justify-content-center text-center">
-				<a class="dropdown-toggle d-flex align-items-center text-decoration-none text-black" href="#" role="button" id="filterDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					<i class="gg-format-center"></i> &nbsp; Filter by Post Status
-				</a>
-				<ul class="dropdown-menu" aria-labelledby="filterDropdown">
-					<li><a class="dropdown-item" href="#" onclick="">Accepted</a></li>
-					<li><a class="dropdown-item" href="#" onclick="">Revised</a></li>
-					<li><a class="dropdown-item" href="#" onclick="">Completed</a></li>
-				</ul>
-			</div>
-			<br>
-
-
-			<div class="container">
-				<div class="row">
-					<div class="col-12">
-						<div class="card border-0 shadow">
-							<div class="card-header bg-white">
-								<h5 class="card-title fw-semibold">Post Title</h5>
-							</div>
-							<div class="card-body">
-								<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eos non commodi obcaecati molestias qui ab? Sed inventore, nam consequuntur dicta dignissimos eaque doloremque, tempore qui necessitatibus, sequi assumenda ducimus?</p>
-							</div>
-							<div class="card-footer bg-white">
-								<button class="btn btn-outline-primary"><i class="fa-solid fa-thumbs-up"></i> Like</button>
-								<button class="btn btn-light"><i class="fa-solid fa-comment"></i> Comment</button>
-								<span class="badge bg-info fs-6">Post Status: Placeholder</span>
-							</div>
-						</div>
+			<div class="col-12">
+				<div class="card border-0 shadow">
+					<div class="card-header bg-white">
+						<h5 class="card-title fw-semibold">Post Title</h5>
+					</div>
+					<div class="card-body">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eos non commodi obcaecati molestias qui ab? Sed inventore, nam consequuntur dicta dignissimos eaque doloremque, tempore qui necessitatibus, sequi assumenda ducimus?</p>
+					</div>
+					<div class="card-footer bg-white">
+						<button class="btn btn-outline-primary"><i class="fa-solid fa-thumbs-up"></i> Like</button>
+						<button class="btn btn-light"><i class="fa-solid fa-comment"></i> Comment</button>
+						<span class="badge bg-info fs-6">Post Status: Placeholder</span>
 					</div>
 				</div>
 			</div>
+
+			<div class="col-12">
+				<div class="card border-0 shadow">
+					<div class="card-header bg-white">
+						<h5 class="card-title fw-semibold">Post Title</h5>
+					</div>
+					<div class="card-body">
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eos non commodi obcaecati molestias qui ab? Sed inventore, nam consequuntur dicta dignissimos eaque doloremque, tempore qui necessitatibus, sequi assumenda ducimus?</p>
+					</div>
+					<div class="card-footer bg-white">
+						<button class="btn btn-outline-primary"><i class="fa-solid fa-thumbs-up"></i> Like</button>
+						<button class="btn btn-light"><i class="fa-solid fa-comment"></i> Comment</button>
+						<span class="badge bg-info fs-6">Post Status: Placeholder</span>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
 
@@ -85,17 +110,41 @@ require "./Middleware/Authenticate.php";
 					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
 				<div class="modal-body">
-					<form action="#" class="needs-validation" method="post" novalidate>
-						<div>
+					<form action="./Controllers/PostController.php" class="needs-validation row g-3" method="post" enctype="multipart/form-data" novalidate>
+
+						<div class="col-12 d-none">
+							<label for="user_id" class="form-label">User ID</label>
+							<input type="text" class="form-control" value="<?php echo $_SESSION['user_id']; ?>" name="user_id" id="user_id" required>
+							<div class="invalid-feedback">
+								You are disallowed to modify this field.
+							</div>
+						</div>
+
+						<div class="col-12">
 							<label for="post_title" class="form-label">Post Title</label>
 							<input type="text" class="form-control" name="post_title" id="post_title" required>
 							<div class="invalid-feedback">
 								Please enter post title.
 							</div>
 						</div>
-						<div>
+
+						<div class="col-12">
+							<label for="post_category">Category</label>
+							<select class="form-select" name="post_category" id="post_category" required>
+								<option value="" hidden selected></option>
+								<option value="qna">QNA</option>
+								<option value="announcement">Annoucement</option>
+								<option value="sharing">Sharing</option>
+								<option value="others">Others</option>
+							</select>
+							<div class="invalid-feedback">
+								Please select a category.
+							</div>
+						</div>
+
+						<div class="col-12">
 							<label for="post_content">Post Content</label>
-							<textarea name="post_content" id="post_content" class="form-control" rows="10" required></textarea>
+							<textarea name="post_content" id="post_content" class="form-control" rows="5" required></textarea>
 							<div class="invalid-feedback">
 								Please enter post content.
 							</div>
