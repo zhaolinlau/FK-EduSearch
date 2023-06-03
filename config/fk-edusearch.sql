605320ae2b99133365123d70392bd581a36bd5a3
2gblue <107160038+2gblue@users.noreply.github.com>
2023-06-04 01:23:49 +0800

    added expert related attributes + publication table

--- config/fk-edusearch.sql
+++ config/fk-edusearch.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2023 at 12:00 PM
+-- Generation Time: Jun 03, 2023 at 07:20 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -84,19 +84,6 @@ CREATE TABLE `feedback` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `likes`
---
-
-CREATE TABLE `likes` (
-  `LikeID` bigint(255) NOT NULL,
-  `UserID` bigint(255) NOT NULL,
-  `PostID` bigint(255) NOT NULL,
-  `Liked_On` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `post`
 --
 
@@ -104,11 +91,23 @@ CREATE TABLE `post` (
   `PostID` bigint(255) NOT NULL,
   `UserID` bigint(255) NOT NULL,
   `PostStatus` tinyint(1) NOT NULL,
-  `PostTitle` varchar(25) NOT NULL,
-  `PostContent` varchar(255) NOT NULL,
+  `PostLike` int(255) NOT NULL,
   `PostCategory` varchar(25) NOT NULL,
   `PostCreated` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
   `PostUpdated` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publication`
+--
+
+CREATE TABLE `publication` (
+  `PublicationID` bigint(255) NOT NULL,
+  `PublicationTitle` varchar(255) NOT NULL,
+  `PublicationDate` varchar(255) NOT NULL,
+  `UserID` bigint(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -138,9 +137,14 @@ CREATE TABLE `user` (
   `UserResearchArea` varchar(100) NOT NULL,
   `StaffID` varchar(10) NOT NULL,
   `StudentID` varchar(10) NOT NULL,
-  `ExpertID` varchar(10) NOT NULL,
   `UserRole` tinyint(1) NOT NULL,
   `ResearchTopic` varchar(25) NOT NULL,
+  `ExpertID` varchar(10) NOT NULL,
+  `ExpertAreaOfExpertise` varchar(255) NOT NULL,
+  `ExpertCV` varchar(255) NOT NULL,
+  `ExpertAccountStatus` int(11) NOT NULL,
+  `ExpertRatings` float NOT NULL,
+  `PublicationID` varchar(255) NOT NULL,
   `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
   `updated_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -149,23 +153,15 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserID`, `UserName`, `UserPassword`, `UserEmail`, `UserSocialMedia`, `UserResearchArea`, `StaffID`, `StudentID`, `ExpertID`, `UserRole`, `ResearchTopic`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '$2y$10$KNL1l8BZGYGbtA63C.ikpuevRBo3OIsZQ3ITuZ2LkACh8B5mxptSy', 'admin@gmail.com', '', '', 'STA001', '0', '', 0, '', '2023-04-21 04:33:09.909294', '2023-05-14 01:51:35.645232'),
-(2, 'expert', '$2y$10$oqnJGRxriBE8NJli9yice.P4..48apl7fH2EmuC0rxljahoD3mey6', 'expert@gmail.com', '', '', '', '0', 'EXB023', 1, '', '2023-04-21 04:40:21.719293', '2023-05-20 13:09:09.951685'),
-(3, 'lecturer', '$2y$10$u3lUUWPNcQD3a5rz3IhkfeNg1asAuRe766JDH4bJGtlWj524jUyEq', 'lecturer@gmail.com', '', '', 'STH750', '0', '', 2, '', '2023-04-21 04:48:28.248413', '2023-05-14 01:51:35.650834'),
-(4, 'student', '$2y$10$DhERISDlz/5dIwsWnn/bNO3TrHu3ET9uALXWwjaLRzaI2mlITvtLW', 'student@gmail.com', '', '', '0', 'CB22039', '', 3, '', '2023-04-21 04:49:40.980236', '2023-05-14 01:51:35.653402');
+INSERT INTO `user` (`UserID`, `UserName`, `UserPassword`, `UserEmail`, `UserSocialMedia`, `UserResearchArea`, `StaffID`, `StudentID`, `UserRole`, `ResearchTopic`, `ExpertID`, `ExpertAreaOfExpertise`, `ExpertCV`, `ExpertAccountStatus`, `ExpertRatings`, `PublicationID`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '$2y$10$KNL1l8BZGYGbtA63C.ikpuevRBo3OIsZQ3ITuZ2LkACh8B5mxptSy', 'admin@gmail.com', '', '', 'STA001', '0', 0, '', '', '', '', 0, 0, '', '2023-04-21 04:33:09.909294', '2023-05-14 01:51:35.645232'),
+(2, 'expert', '$2y$10$oqnJGRxriBE8NJli9yice.P4..48apl7fH2EmuC0rxljahoD3mey6', 'expert@gmail.com', '', '', 'STB023', '0', 1, '', '', '', '', 0, 0, '', '2023-04-21 04:40:21.719293', '2023-05-14 01:51:35.649203'),
+(3, 'lecturer', '$2y$10$u3lUUWPNcQD3a5rz3IhkfeNg1asAuRe766JDH4bJGtlWj524jUyEq', 'lecturer@gmail.com', '', '', 'STH750', '0', 2, '', '', '', '', 0, 0, '', '2023-04-21 04:48:28.248413', '2023-05-14 01:51:35.650834'),
+(4, 'student', '$2y$10$DhERISDlz/5dIwsWnn/bNO3TrHu3ET9uALXWwjaLRzaI2mlITvtLW', 'student@gmail.com', '', '', '0', 'CB22039', 3, '', '', '', '', 0, 0, '', '2023-04-21 04:49:40.980236', '2023-05-14 01:51:35.653402');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `bug`
---
-ALTER TABLE `bug`
-  ADD PRIMARY KEY (`BugID`),
-  ADD UNIQUE KEY `BugID` (`BugID`),
-  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `comment`
@@ -173,7 +169,8 @@ ALTER TABLE `bug`
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`CommentID`),
   ADD UNIQUE KEY `CommentID` (`CommentID`),
-  ADD KEY `PostID` (`PostID`,`UserID`);
+  ADD KEY `PostID` (`PostID`,`UserID`),
+  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `complaint`
@@ -194,20 +191,18 @@ ALTER TABLE `feedback`
   ADD KEY `PostID_2` (`PostID`);
 
 --
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`LikeID`),
-  ADD UNIQUE KEY `LikeID` (`LikeID`),
-  ADD KEY `UserID` (`UserID`,`PostID`);
-
---
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`PostID`),
   ADD UNIQUE KEY `PostID` (`PostID`),
   ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `publication`
+--
+ALTER TABLE `publication`
+  ADD PRIMARY KEY (`PublicationID`);
 
 --
 -- Indexes for table `rating`
@@ -223,19 +218,11 @@ ALTER TABLE `rating`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`UserID`),
   ADD UNIQUE KEY `UserName` (`UserName`),
-  ADD UNIQUE KEY `UserID` (`UserID`),
-  ADD UNIQUE KEY `UserEmail` (`UserEmail`),
-  ADD UNIQUE KEY `StaffID` (`StaffID`,`StudentID`,`ExpertID`);
+  ADD UNIQUE KEY `UserID` (`UserID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `bug`
---
-ALTER TABLE `bug`
-  MODIFY `BugID` bigint(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comment`
@@ -256,16 +243,16 @@ ALTER TABLE `feedback`
   MODIFY `FeedbackID` bigint(255) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `LikeID` bigint(255) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
   MODIFY `PostID` bigint(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `publication`
+--
+ALTER TABLE `publication`
+  MODIFY `PublicationID` bigint(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `rating`
