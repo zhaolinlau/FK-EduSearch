4868a07fb9540146213e44efd5a3c8aa1d0b1f56
Zhaolin Lau <zhaolinlau@gmail.com>
2023-06-04 05:29:53 +0800

    fix create post function

--- controllers/CreatePostController.php
+++ controllers/CreatePostController.php
@@ -7,10 +7,10 @@ try {
 
   $stmt = $conn->prepare('INSERT INTO post (UserID, PostTitle, PostContent, PostCategory) VALUES (:user_id, :post_title, :post_content, :post_category)');
 
-  $stmt = bindParam(':user_id', $_POST['user_id']);
-  $stmt = bindParam(':post_title', $_POST['post_title']);
-  $stmt = bindParam(':post_content', $_POST['post_content']);
-  $stmt = bindParam(':post_category', $_POST['post_category']);
+  $stmt->bindParam(':user_id', $_POST['user_id']);
+  $stmt->bindParam(':post_title', $_POST['post_title']);
+  $stmt->bindParam(':post_content', $_POST['post_content']);
+  $stmt->bindParam(':post_category', $_POST['post_category']);
 
   $stmt->execute();
 
