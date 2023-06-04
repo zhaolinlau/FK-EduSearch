412c49097d63ef47102ec13c0bc13c4c8f0f721f
Zhaolin Lau <zhaolinlau@gmail.com>
2023-06-05 03:44:03 +0800

    change <a> to button

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -13,10 +13,10 @@
 		</form>
 
 		<div class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			<button class="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 				<i class="fa-solid fa-user fa-xl"></i>
 				<span><?php echo $_SESSION["id"] ?></span>
-			</a>
+			</button>
 			<ul class="dropdown-menu dropdown-menu-end shadow-sm">
 				<li><a class="dropdown-item" href="./profile.php">My Profile</a></li>
 				<li><a class="dropdown-item" href="./posts.php">My Posts</a></li>
