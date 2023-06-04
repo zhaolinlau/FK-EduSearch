d8f2ed343d3dcc40082e1daf3c8443245a07b5be
Zhaolin Lau <zhaolinlau@gmail.com>
2023-06-05 02:05:28 +0800

    update create post function

--- controllers/CreatePostController.php
+++ controllers/CreatePostController.php
@@ -5,12 +5,19 @@ require '../config/db.php';
 try {
   session_start();
 
-  $stmt = $conn->prepare('INSERT INTO post (UserID, PostTitle, PostContent, PostCategory) VALUES (:user_id, :post_title, :post_content, :post_category)');
+  $user_id = $_POST['user_id'];
+  $post_title = $_POST['post_title'];
+  $post_content = $_POST['post_content'];
+  $post_category = $_POST['post_category'];
+  $post_status = '-';
 
-  $stmt->bindParam(':user_id', $_POST['user_id']);
-  $stmt->bindParam(':post_title', $_POST['post_title']);
-  $stmt->bindParam(':post_content', $_POST['post_content']);
-  $stmt->bindParam(':post_category', $_POST['post_category']);
+  $stmt = $conn->prepare('INSERT INTO post (UserID, PostTitle, PostContent, PostCategory, PostStatus) VALUES (:user_id, :post_title, :post_content, :post_category, :post_status)');
+
+  $stmt->bindParam(':user_id', $user_id);
+  $stmt->bindParam(':post_title', $post_title);
+  $stmt->bindParam(':post_content', $post_content);
+  $stmt->bindParam(':post_category', $post_category);
+  $stmt->bindParam(':post_status', $post_status);
 
   $stmt->execute();
 
