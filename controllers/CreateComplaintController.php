58cbe83820663047c3c1e6582ac3e057f7767672
Mingkang0 <98515527+Mingkang0@users.noreply.github.com>
2023-06-17 02:22:16 +0800

    Create Complaint& View Fixed

--- controllers/CreateComplaintController.php
+++ controllers/CreateComplaintController.php
@@ -4,13 +4,15 @@ require '../config/db.php';
 try {
     session_start();
 
-    if(isset($_POST['post'])){
+    if (isset($_POST['post'])) {
         $complaint_description = $_POST['complaint_description'];
         $complaint_status = 'In Investigation';
         $feedback_id = $_POST['feedback_id'];
         $complaint_type = $_POST['complaint_type'];
+        $complaint_response = 'No response yet...';
 
-        if(isset($_FILES['fileToUpload'])){
+        // Check if a file is uploaded
+        if (isset($_FILES['fileToUpload']) && $_FILES['fileToUpload']['error'] !== UPLOAD_ERR_NO_FILE) {
             $file = $_FILES['fileToUpload'];
             $fileName = $_FILES['fileToUpload']['name'];
             $fileTmpName = $_FILES['fileToUpload']['tmp_name'];
@@ -19,19 +21,19 @@ try {
             $fileExt = pathinfo($fileName, PATHINFO_EXTENSION); // Get the file extension using pathinfo
             $allowed = array('jpg', 'jpeg', 'png');
 
-            if(in_array($fileExt, $allowed)){
-                if($fileError === 0){
-                    if($fileSize < 1000000){
+            if (in_array($fileExt, $allowed)) {
+                if ($fileError === 0) {
+                    if ($fileSize < 1000000) {
                         $fileNameNew = uniqid('', true) . "." . $fileExt;
                         $fileDestination = '../uploads/' . $fileNameNew;
 
                         // Check if the destination directory exists, if not create it
-                        if(!is_dir('../uploads')){
+                        if (!is_dir('../uploads')) {
                             mkdir('../uploads', 0777, true);
                         }
 
-                        if(move_uploaded_file($fileTmpName, $fileDestination)){
-                            $stmt = $conn->prepare('INSERT INTO complaint (UserID, FeedbackID, ComplaintDescription, ComplaintStatus, ComplaintType, ComplaintPhoto) VALUES (:user_id, :feedback_id, :complaint_description, :complaint_status, :complaint_type, :complaint_photo)');
+                        if (move_uploaded_file($fileTmpName, $fileDestination)) {
+                            $stmt = $conn->prepare('INSERT INTO complaint (UserID, FeedbackID, ComplaintDescription, ComplaintStatus, ComplaintType, ComplaintPhoto, ComplaintResponse) VALUES (:user_id, :feedback_id, :complaint_description, :complaint_status, :complaint_type, :complaint_photo, :complaint_response)');
 
                             $stmt->bindParam(':user_id', $_SESSION['user_id']);
                             $stmt->bindParam(':feedback_id', $feedback_id);
@@ -39,27 +41,29 @@ try {
                             $stmt->bindParam(':complaint_status', $complaint_status);
                             $stmt->bindParam(':complaint_type', $complaint_type);
                             $stmt->bindParam(':complaint_photo', $fileNameNew);
+                            $stmt->bindParam(':complaint_response', $complaint_response);
                             $stmt->execute();
                         } else {
                             echo "Failed to move the uploaded file.";
                         }
-                    } else{
+                    } else {
                         echo "Your file is too big!";
                     }
-                } else{
+                } else {
                     echo "There was an error uploading your file!";
                 }
-            } else{
+            } else {
                 echo "You cannot upload files of this type!";
             }
         } else {
-            $stmt = $conn->prepare('INSERT INTO complaint (UserID, FeedbackID, ComplaintDescription, ComplaintStatus, ComplaintType) VALUES (:user_id, :feedback_id, :complaint_description, :complaint_status, :complaint_type)');
+            $stmt = $conn->prepare('INSERT INTO complaint (UserID, FeedbackID, ComplaintDescription, ComplaintStatus, ComplaintType, ComplaintResponse) VALUES (:user_id, :feedback_id, :complaint_description, :complaint_status, :complaint_type, :complaint_response)');
 
             $stmt->bindParam(':user_id', $_SESSION['user_id']);
             $stmt->bindParam(':feedback_id', $feedback_id);
             $stmt->bindParam(':complaint_description', $complaint_description);
             $stmt->bindParam(':complaint_status', $complaint_status);
             $stmt->bindParam(':complaint_type', $complaint_type);
+            $stmt->bindParam(':complaint_response', $complaint_response);
             $stmt->execute();
         }
 
@@ -72,4 +76,3 @@ try {
 
 $conn = null;
 ?>
-
